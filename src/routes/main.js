50b0d112224c5f1e691c418fc55bd63501bce4d4
Naimad123x <wrx81745@student.wsb.wroclaw.pl>
2023-12-13 12:24:39 +0100

    .env file added

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,12 +1,17 @@
 import * as express from "express";
 const router = express.Router();
+import fetch from "node-fetch";
+import 'dotenv/config'
 
 router.get(`/`, (req, res) =>{
   return res.render(`main`)
 })
 
-router.get(`/:city`, (req, res) =>{
+router.get(`/location/:city`, async (req, res) => {
   const {city} = req.params;
+  const {lang} = req.query || "en";
+
+  await fetch(`/api/location/${city}?lang=${lang}&key=${process.env.API_KEY}`)
   return res.render(`city`)
 })
 
