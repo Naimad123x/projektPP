8df22b1eed08571fd91a0aab6d15c9654f0c9e0d
Naimad123x <wrx81745@student.wsb.wroclaw.pl>
2023-12-19 01:15:08 +0100

    routes\api\info.js update

--- src/site/routes/api/info.js
+++ src/site/routes/api/info.js
@@ -4,9 +4,9 @@ export async function info(req, res){
   return await res.send(
     `<head><title>API Guide</title><link rel="stylesheet" href="/style.css"></head>` +
     `<body><b>Type city name after /api/</b><br>` +
-    `Examples: /api/city/london<br><br>` +
+    `Examples: /api/location/london<br><br>` +
     `<b>You can change language of response by adding query: ?lang={lang}</b><br>` +
-    `Examples: /api/city/london?lang=it<br><br>` +
+    `Examples: /api/location/london?lang=it<br><br>` +
     `<b>Default response format in in JSON. You can change that to xml by adding query ?format=xml</b><br><br>` +
     `<b>Authorization: To use our API you have to use key by adding query ?key={key}</b>` +
     `</body>`
