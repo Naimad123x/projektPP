48eb95c3b968704058ef312e15bbb676134a8d3e
Namadx123 <damianx787@gmail.com>
2024-02-03 17:56:26 +0100

    api/info update

--- src/site/routes/api/info.js
+++ src/site/routes/api/info.js
@@ -7,7 +7,7 @@ export async function info(req, res){
     `Examples: /api/location/london<br><br>` +
     `<b>You can change language of response by adding query: ?lang={lang}</b><br>` +
     `Examples: /api/location/london?lang=it<br><br>` +
-    `<b>Default response format in in JSON. You can change that to xml by adding query ?format=xml</b><br><br>` +
+    `<b>Default response format is JSON. You can change that to xml by adding query ?format=xml</b><br><br>` +
     `<b>Authorization: To use our API you have to use key by adding query ?key={key}</b>` +
     `</body>`
   );
